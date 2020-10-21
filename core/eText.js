1dacd86fbbeeb9f511d0f36d7461900b4d9c1907
lucafont2 <62725689+lucafont2@users.noreply.github.com>
2020-10-21 15:36:01 +0200

    Update eText.js

--- core/eText.js
+++ core/eText.js
@@ -1,3 +1,10 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+http://www.apache.org/licenses/LICENSE-2.0
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 var axios = require("axios");
 var fs = require("fs");
 var qs = require("qs");
