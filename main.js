089e12405fbd9afdb6cc0581d4c09d43fd51a002
lucafont2 <62725689+lucafont2@users.noreply.github.com>
2021-11-21 12:55:49 +0100

    Update main.js
    Adding more logs

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 
 async function yesNo(title){
     return new Promise(async function(re){
-        rl.question("Proseguire con il download di "+ title + "  (y/n) default: n > ", function(yesNo) {
+        rl.question("Do you want to download \""+ title + "\"  (y/n)[default: n]> ", function(yesNo) {
             yesNo = yesNo.toLowerCase();
             re((yesNo == "y"));
         });
@@ -17,12 +17,21 @@ async function yesNo(title){
 
 (async function () {
     var aa = new eText("USERNAME","PASSWORD");
-    await aa.login();
-    var books = await aa.books();
-    for (var i =0;i<books.length;i++){
-        if(await yesNo(books[i].title)){
-            console.log(await aa.download(books[i],__dirname))
-        }
-    }
+	console.log("Doing login...");
+    var lg = await aa.login();
+	if(lg){
+		console.log("Login done, scraping books");
+		var books = await aa.books();
+		console.log("You have " + books.length +" book(s) to download");
+		for (var i =0;i<books.length;i++){
+			if(await yesNo(books[i].title)){
+				await aa.download(books[i],__dirname /* Set this to the directory for the output of the file*/);
+			}
+		}
+		console.log("Nothing more to do, exiting");
+	}else{
+		console.log("Login failed, wrong credentials or you have not accepted the privacy policy (more info on the readme)");
+	}
+    
     process.exit();
 })()
